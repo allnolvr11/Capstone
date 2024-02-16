cafc8a7f8320cf54391d33305450b6d2f750d9cc
allnolvr11 <118269435+allnolvr11@users.noreply.github.com>
2024-02-16 21:32:11 +0800

    updated

--- backend/Database/database.js
+++ backend/Database/database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const db = new Pool({
-    host: 'dpg-cn5hjltjm4es73djn7n7o-a',
+    host: 'dpg-cn5hjltjm4es73dnj7n0-a',
     port: 5432,
     user: 'pakingdb_user',
     password: 'rKhWQG09+ECTNyHguajkUtgLk6bdhnqK',
