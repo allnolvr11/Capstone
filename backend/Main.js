178beb3cd239848044ddadb3323e874b26b37f47
allnolvr11 <118269435+allnolvr11@users.noreply.github.com>
2024-03-04 10:37:18 +0800

    updated

--- backend/Main.js
+++ backend/Main.js
@@ -14,13 +14,11 @@ app.use(bodyParser.json());
 const adminRoute = require('./Routes/Admin')(pool, secretKey);
 const parkingSesRoute = require('./Routes/ParkingSes')(pool, secretKey);
 const ticketRoute = require('./Routes/ticket')(pool, secretKey);
-const ticketRetrievalRoute = require('./Routes/ticketRetrival')(pool, secretKey);
 const parkedVehiclesRoute = require('./Routes/parkedVehicle')(pool, secretKey);
 
 app.use("/api", adminRoute);
 app.use("/api/parkingSession", parkingSesRoute);
 app.use("/api/tickets", ticketRoute);
-app.use("/api/ticketRetrival", ticketRetrievalRoute);
 app.use("/api/parkedVehicles", parkedVehiclesRoute);
 
 app.use(cors());
