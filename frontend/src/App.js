92c64c928ed4332885ddf464fed70774a8e463ba
allnolvr11 <118269435+allnolvr11@users.noreply.github.com>
2024-03-04 11:56:13 +0800

    updated

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
