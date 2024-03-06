4d414b10eba5f35287d615b7feb241c13bf012fa
allnolvr11 <118269435+allnolvr11@users.noreply.github.com>
2024-03-06 11:07:20 +0800

    update with error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import axios from 'axios';
-import { NavBar } from 'react-bootstrap/NavBar';
-import { Container } from 'react-bootstrap/Container';
-import { Row } from 'react-bootstrap/Row';
-import { Col } from 'react-bootstrap/Col';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import  Container  from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from '.react-bootstrap/Nav';
+
 
 import Dashboard from './Components/Dashboard/Dashboard';
 import Login from './Components/Login/Login';
--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import './loginStyle.css';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = JSON.parse(localStorage.getItem('token'));
+        setUser(response.data);
+        navigate("/dashboard");
+      } catch (error) {
+        navigate("/login");
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  // const [token, setToken] = useState(localStorage.getItem('token') || '');
+
+  const handleLogin = async (e) => {
+    try {
+      const response = await axios.post('https://parkingsystem-7aqu.onrender.com/api/login', {
+        username,
+        password
+      });
+      localStorage.setItem('token', JSON.stringify(response.data));
+      navigate('/dashboard');
+    } catch(error) {
+      console.error('Login Failed', error);
+    }
+  };
+
   return (
-    <div>
-      
+    <div className="container">
+      <Row className='vh-100 d-flex justify-content-center align-items-center'>
+        <Col md={8} lg={6} xs={12}>
+          <input type='username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+          <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <Button onClick={handleLogin}>Login</Button>
+        </Col>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
