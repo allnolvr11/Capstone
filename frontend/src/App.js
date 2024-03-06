c4eab5ca185eec474e4a5c0aab60a8703723bf98
allnolvr11 <118269435+allnolvr11@users.noreply.github.com>
2024-03-06 10:22:36 +0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
+import { NavBar } from 'react-bootstrap/NavBar';
+import { Container } from 'react-bootstrap/Container';
+import { Row } from 'react-bootstrap/Row';
+import { Col } from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from '.react-bootstrap/Nav';
 
-import './App.css';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <Container>
+          <Row>
+            <Col md={12}>
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Routes>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
+    </>
   );
 }
 
